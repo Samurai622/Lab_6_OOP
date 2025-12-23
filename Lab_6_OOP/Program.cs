8314cbd88ddbd1f62117264db665102ffbe2dda2
Samurai622 <vovik13on@gmail.com>
2025-12-23 22:45:49 +0200

    Created menu in Program.cs

--- Lab_6_OOP/Program.cs
+++ Lab_6_OOP/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lab_6_OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            while(true)
+            {
+                Console.WriteLine("Оберіть завдання (1-2) або введіть 0 для виходу:");
+                Console.WriteLine("1. Завдання 1: Обробка текстових файлів");
+                Console.WriteLine("2. Завдання 2: Обробка зображень");
+                Console.WriteLine("0. Вихід");
+                
+                string choice = Console.ReadLine();
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case "1":
+                            Task1.Run();
+                            break;
+                        case "2":
+                            Task2.Run();
+                            break;
+                        case "0":
+                            return;
+                        default:
+                            Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Сталася помилка: {ex.Message}");
+                }
+            }
+        }
+    }
+}
