628cd02522d1720e95ba7a8aface435e81e58c13
Samurai622 <vovik13on@gmail.com>
2025-12-24 23:38:28 +0200

    Refacted Task1.cs

--- Lab_6_OOP/Task1.cs
+++ Lab_6_OOP/Task1.cs
@@ -18,32 +18,38 @@ namespace Lab_6_OOP
             foreach (int i in Enumerable.Range(10,20))
             {
                 string Filename = $"{i}.txt";
+                int val1, val2;
+                
                 try
                 {
                     string[] lines = File.ReadAllLines(Filename);
+                    try
+                    {
+                        val1 = int.Parse(lines[0].Trim().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                        val2 = int.Parse(lines[1].Trim().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                    }
+                    catch(Exception ex) when (ex is FormatException || ex  is IndexOutOfRangeException || ex is OverflowException)
+                    {
+                        BD.WriteLine($"Файл {Filename} містить некоректні дані.");
+                        continue;
+                    }
 
-                    int val1 = int.Parse(lines[0].Trim().Split()[0]);
-                    int val2 = int.Parse(lines[1].Trim().Split()[0]);
-
-                    int product = val1 * val2;
-
-                    sum += product;
-                    count++;
+                    try
+                    {
+                        int product = checked(val1 * val2);
+                        sum += product;
+                        count++;
+                    }
+                    catch(OverflowException)
+                    {
+                        OF.WriteLine($"У файлі {Filename} сталася переповнення при обчисленнях.");
+                    }
                 }
                 catch(FileNotFoundException)
                 {
-                    NF.WriteLine(Filename);
-                }
-                catch(Exception ex) when (ex is FormatException || ex is IndexOutOfRangeException)
-                {
-                    BD.WriteLine($"Файл {Filename} містить некоректні дані.");
-                }
-                catch(OverflowException)
-                {
-                    OF.WriteLine($"У файлі {Filename} сталася переповнення при обчисленнях.");
+                    NF.WriteLine($"Файл {Filename} не знайдено.");
                 }
             }
-
             try
             {
                 double average = (double)sum / count;
